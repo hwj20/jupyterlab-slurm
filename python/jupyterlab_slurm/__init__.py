106140697f8cf26564df10c8d4b39cc67a8c2a75
hwj20 <912299489@qq.com>
2025-10-01 19:49:26 -0700

    can run

--- python/jupyterlab_slurm/__init__.py
+++ python/jupyterlab_slurm/__init__.py
@@ -1,13 +1,11 @@
-
-from ._version import __version__
 from .handlers import setup_handlers
+from ._version import __version__
 
 def _jupyter_server_extension_points():
-    return [{
-        "module": "jupyterlab_slurm",
-        "app": "jupyter_server.serverapp.ServerApp",
-    }]
+    # 新 API：告诉 jupyter_server 这个包是扩展模块
+    return [{"module": "jupyterlab_slurm"}]
 
-def _load_jupyter_server_extension(server_app):
+def load_jupyter_server_extension(server_app):
+    # 兼容旧 API 的函数名（没有下划线！）
     setup_handlers(server_app.web_app)
     server_app.log.info("jupyterlab-slurm | server extension loaded")
